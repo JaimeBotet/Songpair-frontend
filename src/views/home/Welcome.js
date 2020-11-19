5ca375d13e0c87c19a4eb0e2296b3022c40ee18c
Guilherme Carra <gui_gc@hotmail.com>
2020-11-19 16:21:41 +0100

    Add Welcome view and styles

--- src/views/home/Welcome.js
+++ src/views/home/Welcome.js
@@ -1,19 +1,45 @@
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import { Container, Row, Col} from 'react-bootstrap';
+import { MusicNoteBeamed } from 'react-bootstrap-icons';
+import LeftLayers from '../components/Transition/LeftLayers'
 import './Welcome.scss';
 
 function Welcome() {
-    return (
-      <Container fluid>
-        <Row>
-          <Col xs={12}>
-            <h1 className="text-white">Soundpair</h1>
-          </Col>
-					<Col xs={8} md={4} lg={2} className="text-white btn btn-primary">Start</Col>
-					<Col xs={12} className="text-center text-secondary">find others favorite songs</Col>
-					<Col xs={12} className="text-center text-secondary">share yours</Col>
-        </Row>
-      </Container>
-    );
-  }
+	const [transition, setTransition] = useState(false);
+	const history = useHistory();
+
+	const handleClick = () => {
+		setTransition(true);
+		setTimeout(() => history.push("/login"), 1500);
+	}
+
+	return (
+		<Container fluid>
+			<LeftLayers active={transition} />
+			<Row>
+				<Col xs={8} md={4} lg={1}>
+					<div className="music-icon move-left">
+						<MusicNoteBeamed size={25} />
+					</div>
+					<div className="music-icon move-right">
+						<MusicNoteBeamed size={25} />
+					</div>
+				</Col>
+				<Col xs={12}>
+					<h1 className="text-white">Soundpair</h1>
+				</Col>
+				<Col xs={8} md={4} lg={2}
+					className="text-white btn btn-primary"
+					onClick={handleClick}
+				>
+					Start
+				</Col>
+				<Col xs={12} className="text-center text-secondary">find others favorite songs</Col>
+				<Col xs={12} className="text-center text-secondary">share yours</Col>
+			</Row>
+		</Container>
+	);
+}
 
 export default Welcome;
