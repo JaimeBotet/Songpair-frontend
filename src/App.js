de8c575b8aedb07cc142a6e1a2cd7dbacb789bf3
Guilherme Carra <gui_gc@hotmail.com>
2020-11-20 12:17:16 +0100

    Add map route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import Auth from './routers/Auth'
 import Home from './routers/Home'
+import Map from './routers/Map'
 
 function App() {
   return (
@@ -14,6 +15,10 @@ function App() {
 
           <Route path="/login">
             <Auth />
+          </Route>
+
+          <Route path="/map">
+            <Map />
           </Route>
 
         </Switch>
