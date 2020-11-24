bda54dbaba15c8019545e92bb45c6cd72794130d
Guilherme Carra <gui_gc@hotmail.com>
2020-11-24 18:34:02 +0100

    Add URI for signup and sigin for server

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,10 @@
 // API domain URL
 export const apiDomain = "http://localhost:8080/";
+
+// API sign up redirection
+export const signUpRedirect = apiDomain + "signup";
+
+// API Requests
+
+// New sign up form request
+export const signInFormURI = apiDomain + "signup/spotify";
