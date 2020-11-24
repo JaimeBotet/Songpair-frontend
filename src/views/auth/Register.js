1600acacafbcd44e67c6c79751e3cb143416a723
Guilherme Carra <gui_gc@hotmail.com>
2020-11-24 18:36:14 +0100

    Add view for Register

--- src/views/auth/Register.js
+++ src/views/auth/Register.js
@@ -1,11 +1,49 @@
+import React, { useEffect } from "react";
+import { useLocation } from 'react-router-dom';
+import { parse } from 'qs';
+import { Container, Row, Col, Form, Spinner} from 'react-bootstrap';
 import './Register.scss';
 
-function Register() {
-    return (
-      <>
-        <h1>Register</h1>
-      </>
-    );
-  }
+function Register({
+  currentUserState: { signInForm, signInFormLoad, signInFormError } = {},
+  fetchFormData
+}) {
+
+  const location = useLocation();
+
+  useEffect(() => {
+    let code = parse(location.search, { ignoreQueryPrefix: true }).code
+    fetchFormData(code)
+  }, []);
+
+  return (
+    <Container fluid className="fade-in register-page">
+      <Row>
+        <Col xs={12}>
+          <h1 className="text-white">Sign up</h1>
+        </Col>
+        {signInFormLoad && !signInForm &&
+          <Spinner animation="border" variant="primary" />
+        }
+        {signInForm &&
+          <Col xs={8} md={5} lg={3} className="text-center">
+            <Form.Control type="file" id="avatar" className="d-none"></Form.Control>
+              <label for="avatar"><img src={signInForm.images[0].url} alt="user avatar" className="avatar" /></label>
+            <Form.Control type="name" placeholder="Your name" className="mb-1" value={signInForm.display_name}></Form.Control>
+            <Form.Control type="email" placeholder="Email" className="mb-1" value={signInForm.email}></Form.Control>
+            <Form.Control type="password" placeholder="Password"></Form.Control>
+            <div className="text-white btn btn-primary w-100">Register</div>
+          </Col>
+        }
+        {signInFormError &&
+        <>
+          <Col xs={12} className="badge badge-danger">{signInFormError}</Col>
+          <Col xs={12} className="text-white text-center">Please try again</Col>
+        </>
+        }
+      </Row>
+    </Container>
+  );
+}
 
 export default Register;
