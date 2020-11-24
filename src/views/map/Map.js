ea7e621d910ebe973fbfc34aec6429b2cf4c7731
Guilherme Carra <gui_gc@hotmail.com>
2020-11-24 11:59:53 +0100

    Update map component

--- src/views/map/Map.js
+++ src/views/map/Map.js
@@ -1,6 +1,7 @@
-import { MapContainer as Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer as Map, TileLayer } from 'react-leaflet';
 import { Container, Row, Col} from 'react-bootstrap';
 import Header from '../components/Header/Header'
+import Marker from '../components/Marker/Marker'
 import './Map.scss';
 import "leaflet/dist/leaflet.css";
 
@@ -9,7 +10,7 @@ function MapView() {
 		<>
 		<Header />
 		<Container fluid className="map-page">
-			<Row>
+			<Row className="map-row">
 				<Col xs={12} md={11} lg={11} xl={4}>
 					<Map
 						className="map-container"
@@ -20,6 +21,7 @@ function MapView() {
 							url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 							attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 						/>
+						<Marker lat={41.390205} long={2.154007} />
 					</Map>
 				</Col>
 			</Row>
