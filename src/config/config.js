ad3c7804675bcf396e23746b8deb6d2d693e12ce
JaimeBotet <jaime.90.botet@gmail.com>
2020-11-30 10:56:23 +0100

    Change SingUpRedirect URI

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,10 @@
 // API domain URL
 export const apiDomain = "http://localhost:8080/";
 
+export const AppDomain = "http://localhost:3000/"
+
 // API sign up redirection
-export const signUpRedirect = apiDomain + "signup";
+export const signUpRedirect = AppDomain + "signup";
 
 // API Requests
 
