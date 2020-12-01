8383916df7d4a2f9eb859a9b26ca0aa48a9ffb40
Guilherme Carra <gui_gc@hotmail.com>
2020-12-01 10:24:28 +0100

    Add: redux variables to Register page container

--- src/redux/containers/pages/RegisterContainer.js
+++ src/redux/containers/pages/RegisterContainer.js
@@ -1,7 +1,7 @@
 import { connect } from "react-redux";
 
 import { currentUserStateSelector } from "../../user/user-selectors";
-import { signInForm } from "../../user/user-actions";
+import { signInForm, signUp } from "../../user/user-actions";
 
 import Signup from "../../../views/auth/Register";
 
@@ -12,6 +12,8 @@ const mapStateToProps = (state) => ({
 const mapDispatchToProps = (dispatch) => ({
   fetchFormData: (code) =>
     dispatch(signInForm(code)),
+  signup: ({name, email, password, avatar, token, refreshToken, location, spotifyID}) =>
+    dispatch(signUp({name, email, password, avatar, token, refreshToken, location, spotifyID})),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Signup);
