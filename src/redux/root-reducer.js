d474a4cefb53bbdca85371dedd40603c658ca43c
Guilherme Carra <gui_gc@hotmail.com>
2020-12-03 15:46:25 +0100

    Add: reducer for community

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -2,10 +2,12 @@ import { combineReducers } from "redux";
 
 import UserReducer from "./user/user-reducer";
 import SongReducer from "./song/song-reducer";
+import CommunityReducer from "./community/community-reducer";
 
 const rootReducer = combineReducers({
   user: UserReducer,
   song: SongReducer,
+  community: CommunityReducer,
 });
 
 export default rootReducer;
