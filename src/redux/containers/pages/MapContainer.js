5caf9cb9eeb8592b3d1c58b585e16b6a0258a6e2
Guilherme Carra <gui_gc@hotmail.com>
2020-12-03 15:47:36 +0100

    Update: map cotainer to have community states

--- src/redux/containers/pages/MapContainer.js
+++ src/redux/containers/pages/MapContainer.js
@@ -1,19 +1,23 @@
 import { connect } from "react-redux";
 
-import {
-  songStateSelector,
-} from "../../song/song-selectors";
+import { currentUserStateSelector } from "../../user/user-selectors";
+import { songStateSelector } from "../../song/song-selectors";
+import { communityStateSelector } from "../../community/community-selectors";
 
 import { fetchSong } from "../../song/song-actions";
+import { fetchNearPeople } from "../../community/community-actions";
 
 import Map from "../../../views/map/Map";
 
 const mapStateToProps = (state) => ({
+  currentUserState: currentUserStateSelector(state),
   songState: songStateSelector(state),
+  communityState: communityStateSelector(state),
 });
 
 const mapDispatchToProps = (dispatch) => ({
   fetchSong: () => dispatch(fetchSong()),
+  fetchNearPeople: () => dispatch(fetchNearPeople()),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Map);
