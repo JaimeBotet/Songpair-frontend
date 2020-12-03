45671664240745e34666b0707f8863b55449d3bc
Guilherme Carra <gui_gc@hotmail.com>
2020-12-03 11:21:08 +0100

    Create: Dashboard view

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -1,6 +1,8 @@
-import { Redirect } from "react-router-dom";
-// import { Container, Row, Col, Form, Spinner} from 'react-bootstrap';
+import { Redirect, Link } from "react-router-dom";
+import { ChatLeftDotsFill, GeoFill, Power } from "react-bootstrap-icons"
+import { Container, Row, Col } from 'react-bootstrap';
 
+import "./Dashboard.scss"
 import ROUTES from "../../utils/routes";
 
 function Dashboard({
@@ -13,7 +15,32 @@ function Dashboard({
   }
 
   return (
-    <h1>Welcome, {currentUser.name}!</h1>
+    <Container fluid className="dashboard py-3">
+        <Row className="header">
+          <Col>
+            <div className="logo">Songpair</div>
+          </Col>
+          <Col className="text-right">
+            <Power size={25} />
+          </Col>
+          <Col xs={12} className="text-center">
+            <img className="avatar" src={currentUser.avatar} alt="user avatar"></img>
+            <span className="username">{currentUser.name}</span>
+          </Col>
+        </Row>
+        <Row className="menu">
+          <Col xs={12}>
+            <Link to={ROUTES.MAP}>
+              <GeoFill color="white" size={100}/>
+            </Link>
+          </Col>
+          <Col xs={12} classname="mt-4">
+            <Link to={ROUTES.CHAT}>
+              <ChatLeftDotsFill color="white" size={100}/>
+            </Link>
+          </Col>
+        </Row>
+    </Container>
   );
 }
 
