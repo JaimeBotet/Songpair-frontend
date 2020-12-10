2cca02de116e5f2f120292eea2bc8135056210cc
Guilherme Carra <gui_gc@hotmail.com>
2020-12-10 11:49:07 +0100

    Add: redux action to update user location

--- src/hooks/useUpdateLocation.js
+++ src/hooks/useUpdateLocation.js
@@ -1,17 +1,20 @@
 import { useState, useEffect } from "react";
 import { positionWatcher } from "../utils/watchPosition";
 
+export default function useUpdateLocation(updateUserLocation){
 
-export default function useUpdateLocation(){
-	const [point, setPoint] = useState(null);
+	const [coords, setCoords] = useState(null);
 
 	useEffect(() => {
-		if (point) {
-			var watcherId = positionWatcher(point, setPoint);
+		if (coords) {
+			var watcherId = positionWatcher(coords, setCoords);
+			updateUserLocation(coords)
+		} else {
+			navigator.geolocation.getCurrentPosition((pos) => setCoords({lat: pos.coords.latitude, long: pos.coords.longitude}) );
 		}
 
 		return function () {
 			navigator.geolocation.clearWatch(watcherId)
 		}
-	}, [point]);
+	}, [coords]);
 }
