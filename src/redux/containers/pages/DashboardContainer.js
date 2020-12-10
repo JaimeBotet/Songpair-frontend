af713ceac7333866cb09c5f80787d2893ad541c0
Guilherme Carra <gui_gc@hotmail.com>
2020-12-10 11:49:41 +0100

    Add: redux action update location to dashboard container

--- src/redux/containers/pages/DashboardContainer.js
+++ src/redux/containers/pages/DashboardContainer.js
@@ -1,7 +1,7 @@
 import { connect } from "react-redux";
 
 import { currentUserStateSelector } from "../../user/user-selectors";
-import { signout } from '../../user/user-actions';
+import { signout, updateUserLocation } from '../../user/user-actions';
 
 import Dashboard from "../../../views/dashboard/Dashboard";
 
@@ -11,6 +11,7 @@ const mapStateToProps = (state) => ({
 
 const mapDispatchToProps = (dispatch) => ({
   logout: () => dispatch(signout()),
+  updateUserLocation: (point) => dispatch(updateUserLocation(point)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
