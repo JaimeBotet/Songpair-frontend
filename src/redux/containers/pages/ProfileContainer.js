36b812c00110c83a4bf0ee482705920b75bbed82
Guilherme Carra <gui_gc@hotmail.com>
2020-12-11 13:35:08 +0100

    Add: getProfile action

--- src/redux/containers/pages/ProfileContainer.js
+++ src/redux/containers/pages/ProfileContainer.js
@@ -1,16 +1,22 @@
 import { connect } from "react-redux";
 
 import { currentUserStateSelector } from "../../user/user-selectors";
-import { signout, updateUserLocation } from '../../user/user-actions';
+import { communityStateSelector } from "../../community/community-selectors";
+
+import { updateUserLocation } from '../../user/user-actions';
+import { getProfile } from '../../community/community-actions';
 
 import Profile from "../../../views/profile/Profile";
 
 const mapStateToProps = (state) => ({
   currentUserState: currentUserStateSelector(state),
+  communityState: communityStateSelector(state),
 });
 
 const mapDispatchToProps = (dispatch) => ({
   updateUserLocation: (point) => dispatch(updateUserLocation(point)),
+  getProfile: (id) => dispatch(getProfile(id)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Profile);
+
