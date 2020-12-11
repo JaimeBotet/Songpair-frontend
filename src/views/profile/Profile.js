cf850942ade7bef4d64d0ba60e6b355920e63876
Guilherme Carra <gui_gc@hotmail.com>
2020-12-11 14:49:08 +0100

    Add: loading screen

--- src/views/profile/Profile.js
+++ src/views/profile/Profile.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { Redirect, useParams } from "react-router-dom";
 import useUpdateLocation from "../../hooks/useUpdateLocation"
 
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Spinner } from 'react-bootstrap';
 import { HeartFill, Heart, MusicNoteList } from 'react-bootstrap-icons';
 import Header from "../components/Header/Header";
 
@@ -12,16 +12,21 @@ import "./Profile.scss";
 function Profile({
   currentUserState: { currentUser, isAuthenticated } = {},
   communityState: { profile, loadingProfile, profileError } = {},
-  updateUserLocation, getProfile
+  updateUserLocation, getProfile, updateLike
 }) {
   const { id } = useParams();
-  const { name, avatar, currentSong} = profile;
+  const { name, avatar, currentSong } = profile;
 
   useUpdateLocation(updateUserLocation);
 
   useEffect(() => {
 		getProfile(id);
-	}, [getProfile, id]);
+  }, [getProfile, id]);
+
+  function handleLike() {
+    updateLike(currentSong, id);
+    currentSong.like = !currentSong.like;
+  }
 
   // Redirect if not logged
   if (!isAuthenticated) {
@@ -39,61 +44,83 @@ function Profile({
               lg={{ span: 6, offset: 3}}
               xl={{ span: 4, offset: 4}}
             >
-              <Row className="profile-page">
-                <Col xs={12} className="avatar">
-                  <img src={avatar} alt="user avatar" />
-                  <h4>{name}</h4>
-                </Col>
-                <Col xs={11}>
-                  <Col xs={12} className="text-center">Currently playing</Col>
-                  <Row className="playing">
-                    { currentSong ? (
-                      <>
-                        <Col xs={3}>
-                          <img src={currentSong.image[0].url} alt="album" />
-                        </Col>
-                        <Col xs={6} xl={7} className="music">
-                          <div className="music-title">{currentSong.music}</div>
-                          <div>{currentSong.artist}</div>
-                        </Col>
-                        <Col xs={3} xl={2} className="music-actions">
-                          <div><HeartFill color="crimson" /><span>101</span></div>
-                          <div><MusicNoteList /><span>Add</span></div>
-                        </Col>
-                      </>
-                      ) : (
-                        <Col xs={12} className="text-center">No song</Col>
-                      )}
+              {!loadingProfile ? (
+                <Row className="profile-page">
+                  <Col xs={12} className="avatar">
+                    <img src={avatar} alt="user avatar" />
+                    <h4>{name}</h4>
+                  </Col>
+                  <Col xs={11}>
+                    <Col xs={12} className="text-center">Currently playing</Col>
+                    <Row className="playing">
+                      { currentSong ? (
+                        <>
+                          <Col xs={3}>
+                            <img src={currentSong.image[0].url} alt="album" />
+                          </Col>
+                          <Col xs={6} xl={7} className="music">
+                            <div className="music-title">{currentSong.music}</div>
+                            <div>{currentSong.artist}</div>
+                          </Col>
+                          <Col xs={3} xl={2} className="music-actions">
+                            <div>
+                              { currentSong.like ? (
+                                <HeartFill
+                                  role="button"
+                                  color="crimson"
+                                  onClick={handleLike}
+                                />
+                                ) : (
+                                <Heart
+                                  role="button"
+                                  color="crimson"
+                                  onClick={handleLike}
+                                />
+                              )}
+                              <span>101</span>
+                            </div>
+                            <div><MusicNoteList /><span>Add</span></div>
+                          </Col>
+                        </>
+                        ) : (
+                          <Col xs={12} className="text-center">No song</Col>
+                        )}
+                      </Row>
+                  </Col>
+                  <Col xs={11}>
+                    <Row className="status">
+                      <Col xs={3} className="total-likes">
+                        <Row>
+                          <Col xs={12}><h5>Total Likes</h5></Col>
+                          <Col xs={12}><HeartFill color="crimson"/></Col>
+                          <Col xs={12}>101</Col>
+                        </Row>
+                      </Col>
+                      <Col xs={8} className="most-liked">
+                        <Row>
+                          <Col xs={12}><h5>Most Liked Song</h5></Col>
+                          <Col xs={6}>
+                            <img src="https://i.scdn.co/image/ab67616d0000b273c6f7af36ecdc3ed6e0a1f169" alt="album" />
+                          </Col>
+                          <Col xs={6} className="music">
+                            <div className="music-title">Dance Monkey</div>
+                            <div>Tones And I</div>
+                            <div><HeartFill color="crimson" /><span>101</span></div>
+                          </Col>
+                        </Row>
+                      </Col>
                     </Row>
-                </Col>
-                <Col xs={11}>
-                  <Row className="status">
-                    <Col xs={3} className="total-likes">
-                      <Row>
-                        <Col xs={12}><h5>Total Likes</h5></Col>
-                        <Col xs={12}><HeartFill color="crimson"/></Col>
-                        <Col xs={12}>101</Col>
-                      </Row>
-                    </Col>
-                    <Col xs={8} className="most-liked">
-                      <Row>
-                        <Col xs={12}><h5>Most Liked Song</h5></Col>
-                        <Col xs={6}>
-                          <img src="https://i.scdn.co/image/ab67616d0000b273c6f7af36ecdc3ed6e0a1f169" alt="album" />
-                        </Col>
-                        <Col xs={6} className="music">
-                          <div className="music-title">Dance Monkey</div>
-                          <div>Tones And I</div>
-                          <div><HeartFill color="crimson" /><span>101</span></div>
-                        </Col>
-                      </Row>
-                    </Col>
-                  </Row>
-                </Col>
-                <Col xs={11} className="text-white btn btn-primary w-100">
-                  Message
-                </Col>
-              </Row>
+                  </Col>
+                  <Col xs={11} className="text-white btn btn-primary w-100">
+                    Message
+                  </Col>
+                </Row>
+              ) : (
+                <Row className="songpair-load">
+                  <Col xs={12}><h2>Songpair</h2></Col>
+                  <Spinner animation="border" variant="primary" />
+                </Row>
+              )}
             </Col>
           </Row>
       </Container>
